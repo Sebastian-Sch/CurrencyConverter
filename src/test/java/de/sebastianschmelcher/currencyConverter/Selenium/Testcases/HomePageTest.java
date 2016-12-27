14dc48fbc8158c78a459c6deb96418741507e5e6
sebastian <sebastian.schmelcher@freenet.de>
2016-12-27 11:29:13 +0100

    use defined port for selenium tests

--- src/test/java/de/sebastianschmelcher/currencyConverter/Selenium/Testcases/HomePageTest.java
+++ src/test/java/de/sebastianschmelcher/currencyConverter/Selenium/Testcases/HomePageTest.java
@@ -2,19 +2,22 @@ package de.sebastianschmelcher.currencyConverter.Selenium.Testcases;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import de.sebastianschmelcher.currencyConverter.Selenium.SeleniumTestBase;
 import de.sebastianschmelcher.currencyConverter.Selenium.PageObjects.HomePage;
 import de.sebastianschmelcher.currencyConverter.Selenium.PageObjects.RegisterPage;
 
-
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class HomePageTest extends SeleniumTestBase
 {
 	@Test
-	public void testDirectBuy()
+	public void testHome()
 	{
 		final HomePage homePage = new HomePage(driver);
 		homePage.open();
